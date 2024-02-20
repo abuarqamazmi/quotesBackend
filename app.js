1de2ea5b258a49dc09fd45c3e4247d983016a383
abuarqamazmi <abuarqamazmi@gmail.com>
2024-02-20 18:14:59 +0530

    changes in the url

--- app.js
+++ app.js
@@ -18,7 +18,7 @@ mongoose.connect(process.env.ATLAS_URL)
 app.use(express.json()) // for parsing application/json
 app.use(express.urlencoded({ extended: true })) // for parsing application/x-www-form-urlencoded
 app.use(cors({
-    origin:["http://localhost:5173"],
+    origin:["https://quotes-frontend-five.vercel.app/"],
 }));
 // seedDB();
 app.use(quotesRoutes);
